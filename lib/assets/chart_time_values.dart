0d25c1ff978459b21be374609c93772c59f6a49d
dKhortiuk <diana.khortiuk@tlvgenerator.com>
2022-05-21 21:08:23 +0300

    initial commit

--- lib/assets/chart_time_values.dart
+++ lib/assets/chart_time_values.dart
@@ -1,14 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:intl/intl.dart';
 
-// DateFormat dateFormat = DateFormat("yyyy-MM-dd");
-// String timeEnd = dateFormat.parse(visibleMaximum.toString()).toIso8601String();
-// String timeStart =
-//     dateFormat.parse(visibleMinimum.toString()).toIso8601String();
-// DateTime visibleMinimum = visibleMaximum.subtract(const Duration(days: 100));
-// DateTime visibleMaximum = DateTime.now();
-// const String period = '1DAY';
-
 @singleton
 class ChartSettings {
   DateFormat getDateFormat() => DateFormat("yyyy-MM-dd");
